1984463dee0d872619ede0ab2ac035dcf764ea1d
Victor Mora <victor.mora.benambourg@gmail.com>
2018-05-08 13:18:07 -0600

    Continued building basics of workshop frontend structure

---
+++ apps/workshop/home/home.jsx
@@ -0,0 +1,32 @@
+/*
+ * Module dependencies
+ */
+import React from 'react'
+
+
+import {connect} from 'react-redux'
+
+@connect((store) => {
+    return {
+    }
+  })
+
+export default class Home extends React.Component {
+
+    componentWillMount() {
+  
+      this.props.dispatch({type: 'HOME_PANEL_MOUNTED', payload: ''})
+  
+    }
+    // *******************************************************************
+  
+    // Main Layout
+    render() {
+  
+      return <div className='Main heigh100'>
+        WORKSHOP HOME
+      </div>
+  
+    }
+  
+  }
--- apps/workshop/main/routes.js
+++ apps/workshop/main/routes.js
@@ -1,2 +1,13 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
+
+//Route components
+
+import Home from '../home/home.jsx'
+
+
+const routes = <div className='heigh100'>
+    <Route exact path='/workshop' component={Home}/>
+</div>
+
+export default routes
