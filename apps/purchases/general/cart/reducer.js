57ceda8ffeb75a7f8c44e3a7e3a308eeafa61528
VictorMora <victor.mora.benambourg@gmail.com>
2018-07-02 11:37:49 -0600

    Purchases now support entering discounts on product lines bt % or by money

--- apps/purchases/general/cart/reducer.js
+++ apps/purchases/general/cart/reducer.js
@@ -10,12 +10,21 @@ const stateConst = {
   cartTotal: 0, // cart total after discount and taxes
   discountTotal: 0, // discount in currency
   cartItemActive: false,
-  orderTransport: 0 //the amount of transport invoiced
+  orderTransport: 0, //the amount of transport invoiced
+  discount_mode: 'percent_based'
 }
 
 export default function reducer(state = stateConst, action) {
 
   switch (action.type) {
+
+    case 'SET_DISCOUNT_MODE':
+    {
+      return {
+        ...state,
+        discount_mode: action.payload
+      }
+    }
 
     case 'SET_TRANSPORT_AMOUNT':
     {
