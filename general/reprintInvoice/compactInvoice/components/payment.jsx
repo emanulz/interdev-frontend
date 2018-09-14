8b1cd6ce4bfab53a05f3e1d6704e838b60dbaa0f
Emanuel Zuniga <emanuelziga@gmail.com>
2018-09-13 23:03:44 -0600

    invoice print complete

--- general/reprintInvoice/compactInvoice/components/payment.jsx
+++ general/reprintInvoice/compactInvoice/components/payment.jsx
@@ -24,6 +24,30 @@ export default class Payment extends React.Component {
     return total
   }
 
+  geyPayCardAmount(payObject) {
+    let total = 0
+    for (const item in payObject.card) {
+      total += payObject.card[item].amount
+    }
+    return total
+  }
+
+  geyPayCreditAmount(payObject) {
+    let total = 0
+    for (const item in payObject.cred) {
+      total += payObject.cred[item].amount
+    }
+    return total
+  }
+
+  geyPayTransferAmount(payObject) {
+    let total = 0
+    for (const item in payObject.tran) {
+      total += payObject.tran[item].amount
+    }
+    return total
+  }
+
   render() {
     const sale = this.props.sale
     // SET THE DEFAULT VALUES
@@ -34,24 +58,58 @@ export default class Payment extends React.Component {
       total = sale.cart.cartTotal
       payObject = sale.pay
     }
+    // DETERMIN THE CASH AMOUNT
     const cashAmount = this.geyPayCashAmount(payObject)
     const cash = cashAmount > 0
       ? <tr>
-        <th>Efectivo</th>
+        <th>Efectivo:</th>
         <td>₡ {cashAmount.formatMoney(2, ',', '.')}</td>
       </tr>
       : ''
-    const changeAmount = cashAmount - total
-    const change = changeAmount > 0.01
+    // DETERMIN THE CARD AMOUNT
+    const cardAmount = this.geyPayCardAmount(payObject)
+    const card = cardAmount > 0
       ? <tr>
-        <th>Vuelto</th>
+        <th>Tarjeta:</th>
+        <td>₡ {cardAmount.formatMoney(2, ',', '.')}</td>
+      </tr>
+      : ''
+
+    // DETERMIN THE TRANSFER AMOUNT
+    const tranAmount = this.geyPayTransferAmount(payObject)
+    const tran = tranAmount > 0
+      ? <tr>
+        <th>Transf:</th>
+        <td>₡ {tranAmount.formatMoney(2, ',', '.')}</td>
+      </tr>
+      : ''
+
+    // DETERMIN THE CREDIT AMOUNT
+    const credAmount = this.geyPayCreditAmount(payObject)
+    const cred = credAmount > 0
+      ? <tr>
+        <th>Crédito:</th>
+        <td>₡ {credAmount.formatMoney(2, ',', '.')}</td>
+      </tr>
+      : ''
+    // DETERMIN THE CHANGE
+    const changeAmount = cashAmount + cardAmount + tranAmount + credAmount - total
+    const change = changeAmount > 0.01
+      ? <tr className='change-row' >
+        <th>Vuelto:</th>
         <td>₡ {changeAmount.formatMoney(2, ',', '.')}</td>
       </tr>
-      : 'Pago Completo'
+      : <tr className='change-row'>
+        <th>Vuelto:</th>
+        <td>Pago Completo</td>
+      </tr>
 
     return <div className='reprint-compact-invoice-payment'>
       <table>
         <tbody>
+          {cred}
+          {tran}
+          {card}
           {cash}
           {change}
         </tbody>
