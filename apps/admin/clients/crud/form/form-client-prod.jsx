d856aef465fc476c153bb5f674f70d6875f47039
VictorMora <victor.mora.benambourg@gmail.com>
2018-09-29 00:18:08 -0600

    Temp styling to add client-prod

--- apps/admin/clients/crud/form/form-client-prod.jsx
+++ apps/admin/clients/crud/form/form-client-prod.jsx
@@ -64,48 +64,55 @@ export default class FormClientProd extends React.Component {
 
         return <div className={clientProdFormClass}>
             <div className='clientProdForm-main'>
-                <div className='clientProdForm-header'>
+                <div className='clientProdForm-main-header'>
                     Crear Entrada Cliente-Producto
                     <i className='fa fa-times' onClick={this.hidePanel.bind(this)}/>
                 </div>
-                <span>Es posible fijar un descuento que será usado contra la lista de precios que corresponda,
-                    o un precio que se usaría por encima de las lista de precios. Esto se controla mendiante la
-                    opción "Fijar mediante precio?" Solo uno de los dos métodos se puede usar en un momento dado.
-                </span>
+
+                <div className="clientProdForm-main-info">
+                    <span>Es posible fijar un descuento que será usado contra la lista de precios que corresponda,
+                        o un precio que se usaría por encima de las lista de precios. Esto se controla mendiante la
+                        opción "Fijar mediante precio?" Solo uno de los dos métodos se puede usar en un momento dado.
+                    </span>
+                </div>
                 <hr/>
 
-                <div className='col-xs-12 row'>
-                    <h2>Detalles del producto</h2>
-                    <span>Código:</span><p>{entry.product_code}</p>
-                    <span>Descripción:</span><p>{entry.product_description}</p>
+                <div className='clientProdForm-main-prodDetails col-xs-12 row'>
+                    <div className="clientProdForm-main-prodDetails-title"><h2>Detalles del producto</h2></div>
+                    <div><span className="clientProdForm-main-prodDetails-title">Código:</span><p className="clientProdForm-main-prodDetails-entry">{entry.product_code}</p></div>
+                    <div><span className="clientProdForm-main-prodDetails-title">Descripción:</span><p className="clientProdForm-main-prodDetails-entry">{entry.product_description}</p></div>
 
                 </div>
 
+                <div className="clientProdForm-main-gridContainer">
 
-                <div className='col-xs-12 row form-container'>
-                    <div className="form-group">
-                        <label>Fijar mediante precio?</label>
-                        <input checked={entry.by_price} type="checkbox" name='by_price' 
-                            onChange={this.handleInputChange.bind(this)}/>
+                    <div className='clientProdForm-main-gridContainer-price col-xs-12 row form-container'>
+                        <div className="clientProdForm-main-gridContainer-price-firstForm form-group">
+                            <label className="clientProdForm-main-gridContainer-price-firstForm-text">Fijar mediante precio?</label>
+                            <input checked={entry.by_price} type="checkbox" name='by_price' 
+                                onChange={this.handleInputChange.bind(this)}/>
                     </div>
 
-                    <div className="form-group">
-                        <label>Descuento</label>
+                    <div className="clientProdForm-main-gridContainer-price-secondForm form-group">
+                        <label className="clientProdForm-main-gridContainer-price-secondForm-text">Descuento</label>
                         <input type="number" value={parseFloat(entry.discount_percent)} 
-                            name='discount_percent'
-                            onChange={this.handleInputChange.bind(this)}/>
+                        name='discount_percent'
+                        onChange={this.handleInputChange.bind(this)}/>
                     </div>
 
-                    <div className="form-group">
-                        <label>Precio de Tabla</label>
+                    <div className="clientProdForm-main-gridContainer-price-thirdForm form-group">
+                        <label className="clientProdForm-main-gridContainer-price-thirdForm-text">Precio de Tabla</label>
                         <input value={entry.table_price} type="number" name="table_price"
-                            onChange={this.handleInputChange.bind(this)}/>
+                        onChange={this.handleInputChange.bind(this)}/>
 
                     </div>
-                    
 
                 </div>
-                <CreateButtons/>
+
+                 <CreateButtons/>   
+
+                </div>
+                
             </div>
         </div>
     }
