3f5066c02c0b889b39007d50d68fdea21a1222ac
VictorMora <victor.mora.benambourg@gmail.com>
2018-10-24 15:04:45 -0600

    Added extra elements to generate the inventory transfer file

---
+++ apps/inventories/fileTransfer/buttons/button.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {generalSave} from '../../../../utils/api.js'
+
+@connect(store=>{
+    return {
+        note: store.extras.notes,
+        cart: store.cart,
+        //pinUser: store.pin,
+        sendToEmails: store.extras.client.email,
+        uniqueId: store.fileTransfer.uniqueId,
+        selectedWarehouse: store.warehouses2.selectedWarehouse.id
+    }
+})
+export default class Buttons extends React.Component {
+
+
+    generateTransfer(){
+        console.log("Generating Inv transfer")
+        const kwargs = {
+            url: '/api/inventorymovementslist/transferInv/',
+            method: 'post',
+            successType: 'INV_DOWNLOAD_SUCCESFUL',
+            errorType: 'INV_DOWNLOAD_REJECTED',
+            sucessMessage: 'Transferencia de inventario a archivo éxitosa',
+            errorMessage: 'Error al transferir inventario a archivo',
+            data: {
+              notes: this.props.notes,
+              cart: JSON.stringify(this.props.cart),
+              transfer_id: this.props.uniqueId,
+              warehouse: this.props.selectedWarehouse 
+            }
+          }
+          this.props.dispatch(generalSave(kwargs))
+    }
+
+    render() {
+        return <div className="col-xs-12 buttons">
+            <button
+                onClick={this.generateTransfer.bind(this)}
+                style={{
+                'height': '48px',
+                'width': '49%',
+                'marginTop': '10px'
+                }}
+                className='btn btn-default'>
+                Generar Transferencia
+                <span>
+                <i className='fa fa-cloud-download' />
+                </span>
+            </button>
+        </div>
+    }
+}
--- apps/inventories/fileTransfer/reducer.js
+++ apps/inventories/fileTransfer/reducer.js
@@ -2,11 +2,20 @@ const stateConst = {
     fullWidth: false,
     clientVisible: false,
     priceListVisible: false,
-    showCurrency: false
+    showCurrency: false,
+    uniqueId: ''
 }
 
 export default function reducer(state=stateConst, action) {
     switch(action.type){
+        case 'SET_UNIQUE_ID':
+        {
+            return {
+                ...state,
+                uniqueId: action.payload
+            }
+        }
+
         case 'TOGGLE_FULL_WIDTH':
         {
           const width = !state.fullWidth
