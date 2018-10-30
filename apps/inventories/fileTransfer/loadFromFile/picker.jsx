2c53ae36726970eec6fdf1c061119a9f1602731c
VictorMora <victor.mora.benambourg@gmail.com>
2018-10-29 19:26:49 -0600

    File picker for inv transfer

--- apps/inventories/fileTransfer/loadFromFile/picker.jsx
+++ apps/inventories/fileTransfer/loadFromFile/picker.jsx
@@ -5,8 +5,29 @@ import {connect} from 'react-redux'
 export default class Picker extends React.Component {
 
 
+    componentWillMount(){
+
+    }
+
+    onFileChange(e){
+        console.log("Archivo seleccionado --> ", e)
+        //this.props.dispatch({type: 'FILE_SELECTED', payload: e.target.files[0]})
+        //this.props.dispatch({type:'SET_IMAGE_FILE', payload:e.target.files[0]})
+    }
 
     render() {
-        return <h1>File picker</h1>
+
+        return <div className='inv-importer'>
+
+            <div className="inv-importer-header">
+            
+                <div>Seleccioné el archivo de transferencia</div>
+                <div className="inv-importer-picker">
+                    <input type="file" name="file_picker" onChange={this.onFileChange.bind(this)} accept='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'/>
+                </div>    
+            
+            </div>
+
+        </div>
     }
 }
---
+++ apps/inventories/fileTransferLoad/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+export default class FileTransferLoader extends React.Component {
+
+
+    componentWillMount(){
+        
+    }
+
+    render(){
+
+        return <div className="transfer-view">
+        
+        
+        </div>
+    }
+}
