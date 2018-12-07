71dbd9af4669e38a39a277b6439ab271afc503d7
VictorMora <victor.mora.benambourg@gmail.com>
2018-12-07 13:17:33 -0600

    Allow downloading zpl from reprint component

--- general/reprintInvoice/reprintInvoicePanel/reprintInvoicePanel.jsx
+++ general/reprintInvoice/reprintInvoicePanel/reprintInvoicePanel.jsx
@@ -50,6 +50,10 @@ export default class ReprintInvoicePanel extends React.Component {
     return total
   }
 
+  getZpl(){
+    document.getElementById('link').click()
+  }
+
   render() {
 
     const isVisible = (this.props.panelVisible)
@@ -74,6 +78,18 @@ export default class ReprintInvoicePanel extends React.Component {
       console.log(err)
     }
 
+    let zpl_button = ''
+    let zpl_link = ''
+    if(this.props.config){
+      if(this.props.config.EnableZplDownloads){
+        if(this.props.sale){
+          zpl_link = <a id="link" href={`/api/saleslist/get_zpl/?sale_consec=${this.props.sale.consecutive}`}></a>
+        }
+      }
+
+      zpl_button = <i onClick={this.getZpl.bind(this)} className='fa fa-download' aria-hidden='true' />
+    }
+
     return <div className={isVisible}>
 
       <div className={'reprint-invoice-panel-main' + isFullClass}>
@@ -86,6 +102,8 @@ export default class ReprintInvoicePanel extends React.Component {
             <i onClick={this.togglePanel.bind(this)} className='fa fa-file-text-o' aria-hidden='true' />
             <i onClick={this.printPanel.bind(this)} className='fa fa-print' aria-hidden='true' />
             {/* <i onClick={this.toggleInvoice.bind(this)} className='fa fa-coffee' aria-hidden='true' /> */}
+            {zpl_button}
+            {zpl_link}
           </div>
         </div>
 
