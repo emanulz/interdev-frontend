ff14974b233a4594b33f3537a843ecd19c0c72fe
Emanuel Zuniga <emanuelziga@gmail.com>
2019-06-10 16:02:21 -0600

    fetchig now stops when clicked if you are staff

--- general/fetching/fetching.jsx
+++ general/fetching/fetching.jsx
@@ -2,13 +2,25 @@
  * Module dependencies
  */
 import React from 'react'
+import {connect} from 'react-redux'
 
+@connect((store) => {
+  return {
+    user: store.user.user
+  }
+})
 export default class Fetching extends React.Component {
+
+  onClickDiv() {
+    const isStaff = this.props.user ? this.props.user.is_staff : false
+    if (isStaff) {
+      this.props.dispatch({type: 'FETCHING_DONE', payload: ''})
+    }
+  }
 
   // Main Layout
   render() {
-
-    return <div className='fetcing-container'>
+    return <div className='fetcing-container' onClick={this.onClickDiv.bind(this)}>
       <img src={'/static/vendor/loaders/Eclipse.gif'} />
       <h1>Cargando elementos</h1>
     </div>
