8d9446fd883e5c97f8c580b5862821f5315ce93f
emanulz <emanuelziga@gmail.com>
2019-07-17 18:50:07 -0600

    Fixed load presales for old taxes

--- apps/sales/sale/presales/actions.js
+++ apps/sales/sale/presales/actions.js
@@ -128,7 +128,7 @@ function checkAndFixCartIVA(cart) {
   cart.cartItems.forEach(line => {
     if (!line.product.taxes_IVA) {
       line.product.tax_code_IVA = '01'
-      if (parseFloat(line.product.taxes) == 0) {
+      if (parseFloat(line.product.taxes) == 0 || !line.product.taxes.use_taxes) {
         line.product.rate_code_IVA = '01'
         line.product.taxes_IVA = '0.00000'
       } else {
