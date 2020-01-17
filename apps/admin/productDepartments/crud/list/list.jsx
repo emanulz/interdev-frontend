67857a78f87c66dcf277e666f8c6d8ea5f4a9413
Emanuel Zuniga <info@interdevcr.com>
2020-01-17 12:21:02 -0600

    fix list and search for department and subdepartment

--- apps/admin/productDepartments/crud/list/list.jsx
+++ apps/admin/productDepartments/crud/list/list.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import AdminTable from '../../../../../general/adminTable/adminTable.jsx'
+import SearchAdmin from '../../../../../general/search/searchAdmin.jsx'
 import { getPaginationItemDispatch } from '../../../../../utils/api.js'
 import Pagination from '../../../../../general/pagination/pagination.jsx'
 import ResultsPerPage from '../../../../../general/pagination/resultsPerPage.jsx'
@@ -13,13 +14,15 @@ import ResultsPerPage from '../../../../../general/pagination/resultsPerPage.jsx
   return {
     fething: store.fetching.fetching,
     productDepartments: store.productDepartments.productDepartments,
-    pageSize: store.pagination.pageSize
+    pageSize: store.pagination.pageSize,
+    searchResults: store.adminSearch.searchResults,
+    paginatedSearchResults: store.adminSearch.paginatedSearchResults
   }
 })
 export default class List extends React.Component {
 
   componentWillMount() {
-
+    this.props.dispatch({type: `adminSearch_CLEAR_SEARCH_RESULTS`, payload: ''})
     this.props.dispatch({type: 'FETCHING_STARTED', payload: ''})
     this.props.dispatch({type: 'CLEAR_PRODUCT_DEPARTMENT', payload: ''})
 
@@ -48,8 +51,47 @@ export default class List extends React.Component {
         text: 'Identificador'
       }
     ]
-
     const fetching = <div />
+    const tableData = this.props.paginated
+      ? this.props.paginatedSearchResults.length ? this.props.paginatedSearchResults : this.props.productDepartments
+      : this.props.searchResults.length ? this.props.searchResults : this.props.productDepartments
+
+    const list = <AdminTable headerOrder={headerOrder} model='department' data={tableData}
+      addLink='/admin/productdepartments/add' idField='id' sortedBy='identifier' />
+
+    const content = this.props.fetching ? fetching : list
+
+    const addLink = <Link className='addBtn' to={'/admin/productdepartments/add'}>
+      <span className='fa fa-plus' />
+      Agregar
+    </Link>
+
+    const paginationDiv = this.props.paginated
+      ? !this.props.paginatedSearchResults.length
+        ? <div className='admin-list-results-pagination' >
+          <ResultsPerPage url='/api/productdepartments/' successType='FETCH_PRODUCT_DEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_DEPARTMENTS_REJECTED' />
+          <Pagination url='/api/productdepartments/' successType='FETCH_PRODUCT_DEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_DEPARTMENTS_REJECTED' />
+        </div>
+        : <PaginatedSearchNavigation namespace='adminSearch' />
+      : !this.props.searchResults.length
+        ? <div className='admin-list-results-pagination' >
+          <ResultsPerPage url='/api/productdepartments/' successType='FETCH_PRODUCT_DEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_DEPARTMENTS_REJECTED' />
+          <Pagination url='/api/productdepartments/' successType='FETCH_PRODUCT_DEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_DEPARTMENTS_REJECTED' />
+        </div>
+        : <div />
+
+    return <div className='list list-container'>
+      <div className='admin-list-header'>
+        <h1>Mantenimiento de Familias:</h1>
+        {addLink}
+      </div>
+
+      <SearchAdmin paginated={this.props.paginated} model='department' namespace='adminSearch' />
+      {paginationDiv}
+
+      {content}
+    </div>
+    /*const fetching = <div />
     const list = <AdminTable headerOrder={headerOrder} model='productDepartments' data={this.props.productDepartments}
       addLink='/admin/productdepartments/add' idField='id' />
 
@@ -76,7 +118,7 @@ export default class List extends React.Component {
         <Pagination url='/api/productdepartments/' successType='FETCH_PRODUCT_DEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_DEPARTMENTS_REJECTED' />
       </div>
       {content}
-    </div>
+    </div>*/
 
   }
 
--- apps/admin/productSubDepartments/crud/list/list.jsx
+++ apps/admin/productSubDepartments/crud/list/list.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import AdminTable from '../../../../../general/adminTable/adminTable.jsx'
+import SearchAdmin from '../../../../../general/search/searchAdmin.jsx'
 import { getPaginationItemDispatch } from '../../../../../utils/api.js'
 import Pagination from '../../../../../general/pagination/pagination.jsx'
 import ResultsPerPage from '../../../../../general/pagination/resultsPerPage.jsx'
@@ -13,13 +14,15 @@ import ResultsPerPage from '../../../../../general/pagination/resultsPerPage.jsx
   return {
     fething: store.fetching.fetching,
     productSubDepartments: store.productSubDepartments.productSubDepartments,
-    pageSize: store.pagination.pageSize
+    pageSize: store.pagination.pageSize,
+    searchResults: store.adminSearch.searchResults,
+    paginatedSearchResults: store.adminSearch.paginatedSearchResults
   }
 })
 export default class List extends React.Component {
 
   componentWillMount() {
-
+    this.props.dispatch({type: `adminSearch_CLEAR_SEARCH_RESULTS`, payload: ''})
     this.props.dispatch({type: 'FETCHING_STARTED', payload: ''})
     this.props.dispatch({type: 'CLEAR_PRODUCT_SUBDEPARTMENT', payload: ''})
 
@@ -48,10 +51,13 @@ export default class List extends React.Component {
         text: 'Identificador'
       }
     ]
-
     const fetching = <div />
-    const list = <AdminTable headerOrder={headerOrder} model='productsubdepartments' data={this.props.productSubDepartments}
-      addLink='/admin/productsubdepartments/add' idField='id' />
+    const tableData = this.props.paginated
+      ? this.props.paginatedSearchResults.length ? this.props.paginatedSearchResults : this.props.productSubDepartments
+      : this.props.searchResults.length ? this.props.searchResults : this.props.productSubDepartments
+
+    const list = <AdminTable headerOrder={headerOrder} model='department' data={tableData}
+      addLink='/admin/productsubdepartments/add' idField='id' sortedBy='identifier' />
 
     const content = this.props.fetching ? fetching : list
 
@@ -60,23 +66,59 @@ export default class List extends React.Component {
       Agregar
     </Link>
 
+    const paginationDiv = this.props.paginated
+      ? !this.props.paginatedSearchResults.length
+        ? <div className='admin-list-results-pagination' >
+          <ResultsPerPage url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
+          <Pagination url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
+        </div>
+        : <PaginatedSearchNavigation namespace='adminSearch' />
+      : !this.props.searchResults.length
+        ? <div className='admin-list-results-pagination' >
+          <ResultsPerPage url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
+          <Pagination url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
+        </div>
+        : <div />
+
     return <div className='list list-container'>
       <div className='admin-list-header'>
-        <h1>Mantenimiento de Sub-Familias de Producto:</h1>
+        <h1>Mantenimiento de Sub Familias:</h1>
         {addLink}
       </div>
-      <div className='admin-list-search'>
-        <input
-          type='text'
-          placeholder='Ingrese un texto para buscar...'
-        />
-      </div>
-      <div className='admin-list-results-pagination' >
-        <ResultsPerPage url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
-        <Pagination url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBSUBDEPARTMENTS_REJECTED' />
-      </div>
+
+      <SearchAdmin paginated={this.props.paginated} model='subdepartment' namespace='adminSearch' />
+      {paginationDiv}
+
       {content}
     </div>
+    // const fetching = <div />
+    // const list = <AdminTable headerOrder={headerOrder} model='productsubdepartments' data={this.props.productSubDepartments}
+    //   addLink='/admin/productsubdepartments/add' idField='id' />
+
+    // const content = this.props.fetching ? fetching : list
+
+    // const addLink = <Link className='addBtn' to={'/admin/productsubdepartments/add'}>
+    //   <span className='fa fa-plus' />
+    //   Agregar
+    // </Link>
+
+    // return <div className='list list-container'>
+    //   <div className='admin-list-header'>
+    //     <h1>Mantenimiento de Sub-Familias de Producto:</h1>
+    //     {addLink}
+    //   </div>
+    //   <div className='admin-list-search'>
+    //     <input
+    //       type='text'
+    //       placeholder='Ingrese un texto para buscar...'
+    //     />
+    //   </div>
+    //   <div className='admin-list-results-pagination' >
+    //     <ResultsPerPage url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBDEPARTMENTS_REJECTED' />
+    //     <Pagination url='/api/productsubdepartments/' successType='FETCH_PRODUCT_SUBDEPARTMENTS_FULFILLED' errorType='FETCH_PRODUCT_SUBSUBDEPARTMENTS_REJECTED' />
+    //   </div>
+    //   {content}
+    // </div>
 
   }
 
